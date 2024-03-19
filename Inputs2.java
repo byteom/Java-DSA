de4092ba602bceda341cc35a74fd9ad115ca8b6e
byteom <123omsinghpipariya@gmail.com>
2024-03-19 15:55:55 +0530

    Update repo

---
+++ Inputs2.java
@@ -0,0 +1,28 @@
+// import java.util.Scanner;
+
+// public class Inputs2 {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter num 1 : ");
+//         int num1 = input.nextInt();
+//         System.out.print("Enter num 2: ");
+//         int num2 = input.nextInt();
+//         int add = num1+num2;
+//         System.out.println("Sum: "+add);
+//     }
+// }
+
+
+import java.util.Scanner;
+
+public class Inputs2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter num 1 : ");
+        float num1 = input.nextFloat();
+        System.out.print("Enter num 2: ");
+        float num2 = input.nextFloat();
+        float add = num1+num2;
+        System.out.println("Sum: "+add);
+    }
+}
